10c01d1203c93acfefd6862a184057ad07a79159
jvazquez30 <josevazquez881@gmail.com>
2024-07-29 15:29:20 -0700

    added SQL code

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,21 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const shipper = await db('shippers').where('shipperid', req.params.id).first()
+  if (!shipper) {
+    next({ status: 404, message: 'that id does not exist'})
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if (!req.body.Phone || !req.body.ShipperName) {
+    next ({ status: 422, message: 'Phone and Shippername are required'})
+  } else {
+    next()
+  }
 }
